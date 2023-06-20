a54839eb3fa89b07d679a12d6de4407249edd346
Mahabub Alahi <mahabubalahi9531@gmail.com>
2023-06-20 23:31:08 +0600

    Update hardhat config file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,49 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { fornoURLs, ICeloNetwork } from "@ubeswap/hardhat-celo";
+import "hardhat-deploy";
+
+import * as dotenv from "dotenv";
+
+import * as path from 'path';
+dotenv.config({ path: path.resolve(__dirname, './.env') });
+
+let keys = [process.env.PRIVATE_KEY!];
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: {
+    version: "0.8.18",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
+  networks: {
+    celo: {
+      chainId: 42220,
+      url: fornoURLs[ICeloNetwork.MAINNET],
+      accounts: keys,
+    },
+    alfajores: {
+      chainId: 44787,
+      url: fornoURLs[ICeloNetwork.ALFAJORES],
+      accounts: keys,
+    },
+  },
+  paths: {
+    tests: "./test/",
+  },
+  mocha: {
+    timeout: 10000000000,
+  },
+  namedAccounts: {
+    deployer: 0
+  },
+  typechain: {
+    outDir: 'typechain',
+  }
 };
 
 export default config;
